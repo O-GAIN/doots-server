8a80c01ddcfae11615e959462ceceee8aea42cf2
shetty-ishan-9 <ishanshetty.10@gmail.com>
2024-08-18 16:55:06 +0530

    server configs

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
-
 const app = express()
+require('dotenv').config()
+
+const PORT = process.env.PORT_URL
 
 app.get('/',(req,res)=>{
     res.send('working')
 })
-app.listen(3000, ()=>{ console.log("app is working")})
+app.listen(PORT, ()=>{ console.log("app is working")})
